7f58756f4665462727ce5c3f4ce091dee395101e
muzammilnawaz <muzammilnawaz2935337@gmail.com>
2025-05-18 10:54:16 -0700

    updated coded

--- index.js
+++ index.js
@@ -5,66 +5,52 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let products = []; 
+let products = [];
+
 app.get('/get-product', (req, res) => {
-    res.json(products)
-})
-app.post('/post-product' , (req, res) => {
-    const {name , price , description} = req.body;
-    if(!name || !price || !description){
-        res.send('requried input field')
-        return
-    } 
-    const product = {
-        id: new Date().getTime(),
-        name,
-        price,
-        description,
-      };
-    products.push(product)
-    res.json(product);
-})
+  res.json(products);
+});
+
+app.post('/post-product', (req, res) => {
+  const { name, price, description, imgUrl } = req.body;
+  if (!name || !price || !description || !imgUrl) {
+    return res.status(400).send('Required input field');
+  }
+  const product = {
+    id: Date.now(),
+    name,
+    price,
+    imgUrl,
+    description,
+  };
+  products.push(product);
+  res.status(201).json(product);
+});
 
 app.delete('/product-delete/:id', (req, res) => {
-    const productId = req.params.id
-    for(let i = 0; i < products.length; i++){
-        if(products[i].id == productId){
-            products.splice(i,1)
-            res.send('product deleted')
-        }
-    }
-})
+  const productId = parseInt(req.params.id);
+  const index = products.findIndex((p) => p.id === productId);
+  if (index === -1) {
+    return res.status(404).send('Product not found');
+  }
+  products.splice(index, 1);
+  res.status(200).send('Product deleted');
+});
 
-app.put('/edit-product/:id' , (req, res) => { 
-    const {name , price , description} = req.body;
-    const productId = req.params.id;
-    if(!name || !price || !description){
-        res.send('requried input field')
-        return
-    }
-    let  isMatched = false
-    for(let i = 0; i < products.length; i++){
-        isMatched = true
-        if(products[i].id == productId){
-            products[i] = {
-                id: productId,
-                name,
-                price,
-                description,
-             }
-             products.splice(i, 1 , {
-               id: productId,
-               name: name,
-               price: price,
-               description: description,
-             })
-             break
-        }}
-        if(isMatched){
-            res.send('product edited')
-        }else{
-            res.send('product  did not deleted')
-        }
-})
+app.put('/edit-product/:id', (req, res) => {
+  const { name, price, description, imgUrl } = req.body;
+  const productId = parseInt(req.params.id);
+  if (!name || !price || !description || !imgUrl) {
+    return res.status(400).send('Required input field');
+  }
+  const index = products.findIndex((p) => p.id === productId);
+  if (index === -1) {
+    return res.status(404).send('Product not found');
+  }
+  products[index] = { id: productId, name, price, imgUrl, description };
+  res.status(200).send('Product updated');
+});
 
-app.listen(5000)
+app.listen(4000, () => {
+  console.log('Server running on http://localhost:4000');
+});
